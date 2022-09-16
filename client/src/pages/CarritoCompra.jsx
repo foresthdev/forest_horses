c3dad9819566c3bed6ff85aaa207556a18c9bbb9
Edurne <edurnenun@gmail.com>
2022-09-16 21:48:26 +0200

    #7 first commit, function CarritoCompra create

---
+++ client/src/pages/CarritoCompra.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
+
+
+function CarritoCompra() {
+  return (
+    <div className="CarritoCompra">
+      <Navbar />
+      
+      <section>
+      <div className="Header">
+        <div className="header-title" > {/*background image con parallax*/}
+            <h1>Cistella</h1>
+        </div>
+        <div>
+          
+        </div>
+   
+    </div>
+      </section>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default CarritoCompra;
